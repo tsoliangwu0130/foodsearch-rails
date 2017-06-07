9c68c78920850317a701fc47fcc331de8dde9cb4
john <johnwu2613@gmail.com>
2017-06-07 18:21:04 +0800

    before-action

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,6 @@
 class BaseController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:callback, :facebook_callback]
+  before_action :current_user
 
   def index
   end
